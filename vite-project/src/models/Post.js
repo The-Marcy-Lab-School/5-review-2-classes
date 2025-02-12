73bba4ce271a6ad58e709bf2871e1800fb659eff
Ben Spector <111444562+benspector-mls@users.noreply.github.com>
2025-02-12 09:42:13 -0500

    Update Post.js

--- vite-project/src/models/Post.js
+++ vite-project/src/models/Post.js
@@ -13,25 +13,47 @@ TIP: Keep track of all Posts in an Object, mapping { id: Post }
 */
 
 class Post {
+  constructor(title, content) {
+    
+  }
 
+  update(newContent) {
+    
+  }
+
+  static getAllPosts() {
+
+  }
+
+  static getPostById(id) {
+
+  }
+  static deletePostById(id) {
+
+  }
 }
 
-export default Post;
+/* 1. We should be able to create new posts and see them */
 
-// We should be able to create new posts and see them
+const posts = [
+  new Post("Understanding Closures", "Closures are functions that remember variables...", "Alice"),
+  new Post("JavaScript Factory Functions", "Factory functions return objects without using `new`...", "Bob"),
+  new Post("Encapsulation in JavaScript", "Encapsulation helps hide data and protect state...", "Charlie")
+]
+console.log(posts);
 
-// new Post("Understanding Closures", "Closures are functions that remember variables...", "Alice");
-// new Post("JavaScript Factory Functions", "Factory functions return objects without using `new`...", "Bob");
-// new Post("Encapsulation in JavaScript", "Encapsulation helps hide data and protect state...", "Charlie");
+/* 2. We should be able to get all posts from the class */
 // console.log(Post.getAllPosts());
 
-// We should be able to find a post and update it
+/* 3. We should be able to find a post by ID and update it */
 
 // const post1 = Post.getPostById(1)
 // post1.update('Closures require an inner function and an outer function.');
 // console.log(Post.getAllPosts());
 
-// We should be able to delete a post
+/* 4. We should be able to delete a post by ID */
 
 // Post.deletePostById(2);
 // console.log(Post.getAllPosts());
+
+export default Post;
